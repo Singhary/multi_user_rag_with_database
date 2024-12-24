aa62cee02251c1eab053914368d50323ded2cc22
Singhary <singhary449@gmail.com>
2024-12-24 11:47:34 +0530

    Refactor SQLite import to use pysqlite3 in chroma_utils.py and remove from app.py

--- app/app.py
+++ app/app.py
@@ -2,9 +2,6 @@ import streamlit as st
 from sidebar import display_sidebar
 from chat_interface import display_chat_interface
 
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 st.title("Langchain RAG Chatbot")
 
