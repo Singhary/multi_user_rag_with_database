0198f092e41bdf09bc064cf86b94dccbd8513a0c
Singhary <singhary449@gmail.com>
2024-12-24 11:32:56 +0530

    Add pysqlite3-binary dependency and adjust sqlite3 import in app.py

--- app/app.py
+++ app/app.py
@@ -2,6 +2,10 @@ import streamlit as st
 from sidebar import display_sidebar
 from chat_interface import display_chat_interface
 
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 st.title("Langchain RAG Chatbot")
 
 if "messages" not in st.session_state:
