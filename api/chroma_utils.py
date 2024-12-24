67ff7ec23b715ee042f430e0c96593af8583a234
Singhary <singhary449@gmail.com>
2024-12-24 15:06:24 +0530

    .

--- api/chroma_utils.py
+++ api/chroma_utils.py
@@ -7,6 +7,10 @@ from langchain_core.documents import Document
 from dotenv import load_dotenv
 import os
 
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 load_dotenv()
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000 , chunk_overlap=200)
--- api/main.py
+++ api/main.py
@@ -19,10 +19,6 @@ logging.basicConfig(filename="app.log", level=logging.INFO)
 
 app = FastAPI()
 
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
 
 @app.post("/chat", response_model=QueryResponse)
 async def chat(query_input: QueryInput):
